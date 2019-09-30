a8d07b60f932e29c4d6bf275fcd1597905bd4cca
Anton Shangareev <shananton2000@gmail.com>
2019-09-30 15:54:01 +0300

    Add Message.kt

---
+++ src/main/kotlin/Message.kt
@@ -0,0 +1,10 @@
+interface Attachment {}
+interface User {}
+class Message(
+    var text: String, val data: Attachment, val from: User, val time: Int,
+    val isDeleted: Boolean, val id: Long
+) {
+    fun edit(user: User, newText: String) {
+        text = newText
+    }
+}
