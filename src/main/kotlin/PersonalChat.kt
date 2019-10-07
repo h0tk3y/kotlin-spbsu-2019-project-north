69eea96cbf59b918eb963bc7d4dc6293c5c0b673
Ivan Oleinik <olivva239@gmail.com>
2019-10-07 15:38:40 +0300

    Fix PersonalChat.

--- src/main/kotlin/PersonalChat.kt
+++ src/main/kotlin/PersonalChat.kt
@@ -3,9 +3,8 @@ import java.util.*
 
 class PersonalChat(val member1: User, val member2: User) : Chat(MutableList(), MutableList()) {
     init {
-        members = MutableListOf(member1, member2)
+        members = mutableListOf(member1, member2)
         member1.addChat(this)
         member2.addChat(this)
     }
-
 }
