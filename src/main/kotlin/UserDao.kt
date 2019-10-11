a70c66ffa2e563d9c38c6c6050838000c4637e37
AlyaNovikova <aleksnovikova@gmail.com>
2019-10-11 21:15:41 +0300

    add list of userLogins

--- src/main/kotlin/UserDao.kt
+++ src/main/kotlin/UserDao.kt
@@ -1,5 +1,7 @@
 typealias UserId = Long
 interface UserDao : Dao<User> {
+    var userLogins: List<String>
+
     fun searchByName(name: String): List<UserId>
     fun getByEmail(email: String): UserId?
     fun getByPhoneNumber(phoneNumber: String): UserId?
