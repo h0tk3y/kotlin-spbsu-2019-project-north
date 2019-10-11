3a6c3734becde452bcf1f033a10db811889adbfe
Anton Shangareev <shananton2000@gmail.com>
2019-10-11 19:03:38 +0300

    Make User a data class

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,7 +1,8 @@
-interface User {
-    val id: Id
-    val name: String
-    val email: String
-    val phoneNumber: String
-    val chats: MutableList<Id>
-}
+data class User(
+    val id: UserId,
+    val name: String,
+    val email: String,
+    val phoneNumber: String,
+    val chats: MutableList<ChatId>,
+    val blockedUsers: MutableList<UserId>
+)
