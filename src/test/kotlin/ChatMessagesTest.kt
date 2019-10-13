d6e2e95a8e5824900eba0d76a6fe5b65bce4d7f6
Nikita Bosov <neckbosov@gmail.com>
2019-10-13 16:30:41 +0300

    Reformat tests

--- src/test/kotlin/ChatMessagesTest.kt
+++ src/test/kotlin/ChatMessagesTest.kt
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 
 class ChatMessagestTest {
-    @Test
-    fun absoluteTruth() {
-        assertEquals(2, 1 + 1, "hmmmmmmmmmmm. 1 + 1 = 2")
-    }
     val message1 = Message(0, "Alya is cool", 0, 0, false, false)
     val message2 = Message(0, "Alya is cool version 2", 0, 0, false, false)
     val base = MessageDB
@@ -19,12 +15,14 @@ class ChatMessagestTest {
         chat.add(message1)
         Assertions.assertTrue(chat.messageList.size == 1)
     }
+
     @Test
     fun testGet() {
         val chat = ChatMessages(base)
         val id = chat.add(message2)
         Assertions.assertTrue(chat.get(id) == message2)
     }
+
     @Test
     fun testModify() {
         val chat = ChatMessages(base)
@@ -32,6 +30,7 @@ class ChatMessagestTest {
         chat.modify(id, message2)
         Assertions.assertTrue(chat.get(id) == message2)
     }
+
     @Test
     fun testDelete() {
         val chat = ChatMessages(base)
@@ -41,6 +40,7 @@ class ChatMessagestTest {
         Assertions.assertTrue(chat.get(id1) == null && chat.get(id2) == message2)
         Assertions.assertTrue(chat.messageList.size == 1)
     }
+
     @Test
     fun testSearchByText() {
         val chat = ChatMessages(base)
--- src/test/kotlin/MessageTest.kt
+++ src/test/kotlin/MessageTest.kt
@@ -1,20 +1,14 @@
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertEquals
-
-
 class MessageTest {
-    @Test
-    fun absoluteTruth() {
-        assertEquals(2, 1 + 1, "hmmmmmmmmmmm. 1 + 1 = 2")
-    }
     @Test
     fun testEdit() {
         val message = Message(0, "My name is Sasha", 0, 0, false, false)
         message.edit("My name is Alya")
         Assertions.assertTrue(message.isEdited && message.text == "My name is Alya")
     }
+
     @Test
     fun testDelete() {
         val message = Message(0, "My name is Sasha", 0, 0, false, false)
