ef338dae15c6e87d8bb6182b60c8f2ba0500eb50
Nikita Bosov <neckbosov@gmail.com>
2019-10-13 15:48:59 +0300

    Refactor ChatDB

--- src/main/kotlin/ChatDB.kt
+++ src/main/kotlin/ChatDB.kt
@@ -1,22 +1,22 @@
-object ChatBase : ChatDao {
-    val base: MutableMap<ChatId, Chat> = hashMapOf()
+object ChatDB : ChatDao {
+    val chats: MutableMap<ChatId, Chat> = hashMapOf()
 
-    override fun add(elem: Chat): Id {
-        val id = base.size.toLong()
-        base.put(id, elem)
+    override fun add(elem: Chat): ChatId {
+        val id = chats.size.toLong()
+        chats.put(id, elem)
         return id
      }
 
-    override fun get(elemId: ChatId): Chat? = base.get(elemId)
+    override fun get(elemId: ChatId): Chat? = chats.get(elemId)
 
     override fun modify(elemId: ChatId, newElem: Chat) {
-        base.put(elemId, newElem)
+        chats.put(elemId, newElem)
     }
 
     override fun delete(elemId: ChatId) {
-        base.remove(elemId)
+        chats.remove(elemId)
     }
-    override fun getChatByInviteLink(link: String): ChatId? = base.entries
+    override fun getChatByInviteLink(link: String): ChatId? = chats.entries
         .find {
              val chat = it.value as? GroupChat
              if (chat == null) {
@@ -25,24 +25,22 @@ object ChatBase : ChatDao {
                  chat.uniqueLink == link
              }
         }?.key
-    override fun searchByName(name: String): List<ChatId> = base.entries
-        .filter {
-            val chat = it.value as? GroupChat
-            if (chat == null) {
-                false
-            } else {
-                chat.chatName == name
+    override fun searchByName(name: String): List<ChatId> = chats.entries
+        .mapNotNull { if ((it.value as? GroupChat)?.chatName?.equals(name) ?: false) it.key else null }
+        
+    override fun searchWithUser(userId: UserId): List<ChatId> = chats.entries
+         .mapNotNull {
+            when (val chat = it.value) {
+                is PersonalChat -> {
+                    if (chat.member1 == userId || chat.member2 == userId) it.key
+                    else null
+                }
+                is GroupChat -> {
+                    if (chat.users.contains(userId)) it.key
+                    else null
+                }
+                else -> null
             }
-         }.map { it.key }.toList()
-    override fun searchWithUser(userId: UserId): List<ChatId> = base.entries
-         .filter {
-             val chat = it.value
-             if (chat is PersonalChat) {
-                  chat.member1 == userId ||
-                  chat.member2 == userId
-             } else {
-                 (chat as GroupChat).users.contains(userId)
-             }
-          }.map { it.key }.toList()
+        }
 }
 
