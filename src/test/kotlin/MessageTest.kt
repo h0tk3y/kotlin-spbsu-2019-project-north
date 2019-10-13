676035a44cfe37eb449b940074ba78fffe2e37bb
AlyaNovikova <aleksnovikova@gmail.com>
2019-10-13 13:25:41 +0300

    tests for class Message

--- src/test/kotlin/MessageTest.kt
+++ src/test/kotlin/MessageTest.kt
@@ -1,8 +1,24 @@
-//import org.junit.Test
-//
-//class messagetests {
-//    @test
-//    fun messageedittest() {
-//        val message msg()
-//    }
-//}
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+
+
+class MessageTest {
+    @Test
+    fun absoluteTruth() {
+        assertEquals(2, 1 + 1, "hmmmmmmmmmmm. 1 + 1 = 2")
+    }
+    @Test
+    fun testEdit() {
+        val message = Message(0, "My name is Sasha", 0, 1, false, false)
+        message.edit("My name is Alya")
+        Assertions.assertTrue(message.isEdited && message.text == "My name is Alya")
+    }
+    @Test
+    fun testDelete() {
+        val message = Message(0, "My name is Sasha", 0, 1, false, false)
+        message.delete()
+        Assertions.assertTrue(message.isDeleted && message.text == "This message has been deleted")
+    }
+}
