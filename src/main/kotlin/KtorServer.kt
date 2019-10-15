ba58e6d886b12ffdb0665f5504ed03f30673432d
Ivan Oleinik <olivva239@gmail.com>
2019-10-15 17:26:10 +0300

    Modify login request.

--- src/main/kotlin/KtorServer.kt
+++ src/main/kotlin/KtorServer.kt
@@ -1,4 +1,3 @@
-
 import io.ktor.application.*
 import io.ktor.features.ContentNegotiation
 import io.ktor.http.*
@@ -9,36 +8,62 @@ import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.sessions.*
+import io.ktor.util.Hash
 import io.ktor.util.toMap
+import jdk.nashorn.internal.objects.NativeDate.getUTCMilliseconds
 import org.json.simple.JSONObject
+import java.security.MessageDigest
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-val DB = mutableMapOf<String, String>("log" to "pass")
-//http://127.0.0.1:8080/login?login=log&password=pass
-//http://127.0.0.1:8080/session
+val DB = mutableMapOf<String, Int>("log" to "pass".hashCode())
+
+private fun hashString(input: String): String {
+    val HEX_CHARS = "0123456789ABCDEF"
+    val bytes = MessageDigest
+        .getInstance("SHA-256")
+        .digest(input.toByteArray())
+    val result = StringBuilder(bytes.size * 2)
+
+    bytes.forEach {
+        val i = it.toInt()
+        result.append(HEX_CHARS[i shr 4 and 0x0f])
+        result.append(HEX_CHARS[i and 0x0f])
+    }
+
+    return result.toString()
+}
+
 fun main(args: Array<String>) {
-    data class IndexData(val value: List<Int>)
-    data class MySession(val name: String?, val value: Int)
+    data class MySession(val name: String?, val time: String)
+    data class Credentials(val login: String?, val passHash: Int?)
 
     val server = embeddedServer(Netty, port = 8080) {
         routing {
-            install(Sessions) { cookie<MySession>("COOKIE_NAME")
+            install(Sessions) {
+                cookie<MySession>("COOKIE_NAME")
             }
             install(ContentNegotiation) {
                 jackson {}
             }
-            get("/login") {
-                val login = call.request.queryParameters["login"]
-                val password = call.request.queryParameters["password"]
+            post("/login") {
+                val creds = call.receive<Credentials>()
+                val login = creds.login
+                val passHash = creds.passHash
                 when {
-                    login == null || password == null -> call.respondText("Логин или пароль null")
-                    login in DB.keys && DB[login] == password -> {
-                        call.sessions.set(MySession(login, 239))
-                        call.respondText("OK")
+                    login == null || passHash == null ->
+                        call.respond(mapOf("status" to "Login or password is missing"))
+                    login in DB.keys && DB[login] == passHash -> {
+                        val time = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+                        val token = hashString(creds.toString() + time)
+                        call.sessions.set(MySession(login, time))
+                        call.respond(mapOf("status" to "OK")) // TODO: CONTENT TYPE
                     }
-                    else -> call.respondText("Неправильный логин или пароль")
+                    else -> call.respond(mapOf("status" to "Неправильный логин или пароль"))
                 }
             }
-            get("/register") {
+            post("/register") {
                 val login = call.request.queryParameters["login"]
                 val password = call.request.queryParameters["password"]
                 when {
@@ -46,18 +71,13 @@ fun main(args: Array<String>) {
                     login in DB.keys -> call.respondText("Пользователь с таким именем уже существует")
                     password.length < 6 -> call.respondText("Пароль слишком короткий")
                     else -> {
-                        DB[login] = password
+                        DB[login] = password.hashCode()
                         call.respondText("OK")
                     }
                 }
             }
-            get("/session") {
-                val session = call.sessions.get<MySession>() ?: MySession(name = "Empty", value = 0)
-                call.sessions.set(session.copy(value = session.value + 1))
-                call.respondText("Current session : ${session.name}")
-            }
             get("/logout") {
-                val session = call.sessions.get<MySession>() ?: MySession(name = "Empty", value = 0)
+                val session = call.sessions.get<MySession>() ?: MySession(name = "Empty", time = "")
                 call.sessions.clear<MySession>()
                 call.respondText("Logged out : ${session.name}")
             }
