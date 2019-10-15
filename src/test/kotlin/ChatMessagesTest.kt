948130a654f7d1489cd74c9c4049c5c00558aefd
Nikita Bosov <neckbosov@gmail.com>
2019-10-15 18:33:32 +0300

    Move kotlin runs in Before* annotative functions

--- src/test/kotlin/ChatMessagesTest.kt
+++ src/test/kotlin/ChatMessagesTest.kt
@@ -1,11 +1,4 @@
-import org.junit.After
-import org.junit.AfterClass
-import org.junit.Before
-import org.junit.BeforeClass
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.koin.core.inject
 import org.koin.test.KoinTest
 import org.koin.core.context.startKoin
@@ -15,37 +8,40 @@ class ChatMessagestTest : KoinTest {
     val message1 = Message(0, "Alya is cool", 0, 0, false, false)
     val message2 = Message(0, "Alya is cool version 2", 0, 0, false, false)
 
+    @BeforeEach
+    fun start() {
+        startKoin { modules(daoModule) }
+    }
+
+    @AfterEach
+    fun stop() {
+        stopKoin()
+    }
+
     @Test
     fun testAdd() {
-        startKoin { modules(daoModule) }
         val chat: ChatMessageDao by inject()
         chat.add(message1)
         Assertions.assertEquals(1, chat.size)
-        stopKoin()
     }
 
     @Test
     fun testGet() {
-        startKoin { modules(daoModule) }
         val chat: ChatMessageDao by inject()
         val id = chat.add(message2)
         Assertions.assertEquals(message2, chat.get(id))
-        stopKoin()
     }
 
     @Test
     fun testModify() {
-        startKoin { modules(daoModule) }
         val chat: ChatMessageDao by inject()
         val id = chat.add(message1)
         chat.modify(id, message2)
         Assertions.assertEquals(message2, chat.get(id))
-        stopKoin()
     }
 
     @Test
     fun testDelete() {
-        startKoin { modules(daoModule) }
         val chat: ChatMessageDao by inject()
         val id1 = chat.add(message1)
         val id2 = chat.add(message2)
@@ -53,12 +49,10 @@ class ChatMessagestTest : KoinTest {
         Assertions.assertEquals(null, chat.get(id1))
         Assertions.assertEquals(message2, chat.get(id2))
         Assertions.assertEquals(1, chat.size)
-        stopKoin()
     }
 
     @Test
     fun testSearchByText() {
-        startKoin { modules(daoModule) }
         val chat: ChatMessageDao by inject()
         val id1 = chat.add(message1)
         val id2 = chat.add(message2)
@@ -66,6 +60,5 @@ class ChatMessagestTest : KoinTest {
         Assertions.assertEquals(chat.searchByText("Alya is cool"), listOf(id1, id2))
         chat.delete(id2)
         Assertions.assertEquals(chat.searchByText("Alya"), listOf(id1))
-        stopKoin()
     }
 }
--- src/test/kotlin/GroupChatTest.kt
+++ src/test/kotlin/GroupChatTest.kt
@@ -1,4 +1,6 @@
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.koin.test.KoinTest
 import org.koin.test.inject
@@ -6,10 +8,18 @@ import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 
 class GroupChatTest : KoinTest {
+    @BeforeEach
+    fun start() {
+        startKoin { modules(daoModule) }
+    }
+
+    @AfterEach
+    fun stop() {
+        stopKoin()
+    }
 
     @Test
     fun testAddUserPublicChat() {
-        startKoin { modules(daoModule) }
         val base: MessageDao by inject()
         val chat = GroupChat(base, 0, "kek", null)
         Assertions.assertEquals(1, chat.countUsers)
@@ -17,12 +27,10 @@ class GroupChatTest : KoinTest {
         Assertions.assertEquals(2, chat.countUsers)
         Assertions.assertFalse(chat.addUser(1, null))
         Assertions.assertEquals(2, chat.countUsers)
-        stopKoin()
     }
 
     @Test
     fun testAddUserPrivateChat() {
-        startKoin { modules(daoModule) }
         val base: MessageDao by inject()
         val chat = GroupChat(base, 0, "lol", "sm.me/lolabacabadaba")
         Assertions.assertEquals(1, chat.countUsers)
@@ -34,19 +42,16 @@ class GroupChatTest : KoinTest {
         Assertions.assertEquals(2, chat.countUsers)
         Assertions.assertFalse(chat.addUser(2, "sm.me/lolabacabadaba"))
         Assertions.assertEquals(2, chat.countUsers)
-        stopKoin()
     }
 
     @Test
     fun testLeave() {
-        startKoin { modules(daoModule) }
         val base: MessageDao by inject()
         val chat = GroupChat(base, 0, "kek", null)
         chat.addUser(1)
         Assertions.assertEquals(2, chat.countUsers)
         chat.leave(1)
         Assertions.assertEquals(1, chat.countUsers)
-        stopKoin()
     }
 }
 
