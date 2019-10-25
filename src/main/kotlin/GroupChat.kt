2363b3e6d31e62b62aac43d62e259aca0714074a
Nikita Bosov <neckbosov@gmail.com>
2019-10-25 14:04:45 +0300

    Remove imports

--- src/main/kotlin/GroupChat.kt
+++ src/main/kotlin/GroupChat.kt
@@ -1,6 +1,3 @@
-import org.koin.core.KoinComponent
-import org.koin.core.inject
-
 data class GroupChat(
     val messageDB: MessageDao,
     val owner: UserId,
--- src/main/kotlin/PersonalChat.kt
+++ src/main/kotlin/PersonalChat.kt
@@ -1,6 +1,3 @@
-import org.koin.core.KoinComponent
-import org.koin.core.inject
-
 data class PersonalChat(
     val member1: UserId,
     val member2: UserId
