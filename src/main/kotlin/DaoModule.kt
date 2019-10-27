4c25873aaf238151c863d3376370ac3abd0f3aa5
Nikita Bosov <neckbosov@gmail.com>
2019-10-27 17:34:53 +0300

    Update DaoModule

--- src/main/kotlin/DaoModule.kt
+++ src/main/kotlin/DaoModule.kt
@@ -4,4 +4,7 @@ val daoModule = module {
     single<MessageDao> { MessageDB() }
     single<UserDao> { UserDB() }
     single<ChatDao> { ChatDB() }
+    single<ChatsOfUserDao> { ChatsOfUserDB() }
+    single<BlockedUsersDao> { BlockedUsersDB() }
+    single<MembersOfGroupChatDao> { MembersOfGroupChatDB() }
 }
