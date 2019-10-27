60d6c40894194809a59aa187fd925e746e1051cf
Egor Gorbachev <peltorator@yandex.ru>
2019-10-27 23:02:16 +0300

    MessageDao: bugfix

--- src/main/kotlin/dao/MessageDao.kt
+++ src/main/kotlin/dao/MessageDao.kt
@@ -6,5 +6,5 @@ typealias MessageId = Long
 
 interface MessageDao : ObjectDao<Message> {
     fun findByUser(user: UserId): List<MessageId>
-    fun findSliceFromChat(chat: ChatId, block: Int, last: Int?): List<MessageId>
+    fun findSliceFromChat(chat: ChatId, block: Int, last: Int? = null): List<MessageId>
 }
