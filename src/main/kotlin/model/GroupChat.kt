da79d3f26cbabb94604051278ca6ad307a4ce26a
Nikita Bosov <neckbosov@gmail.com>
2019-11-08 11:14:20 +0300

    Fix GroupChat

--- src/main/kotlin/model/GroupChat.kt
+++ src/main/kotlin/model/GroupChat.kt
@@ -1,10 +1,8 @@
 package model
 
-import dao.MessageDao
 import dao.UserId
 
 data class GroupChat(
-    val messageDB: MessageDao,
     val owner: UserId,
     val chatName: String,
     val uniqueLink: String? = null
