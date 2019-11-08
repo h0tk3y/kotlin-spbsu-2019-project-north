33359ade373cb1c7c131e7c0b36a84e6ca6ba737
Nikita Bosov <neckbosov@gmail.com>
2019-11-08 10:37:11 +0300

    Add databases

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,22 +1,51 @@
-import dao.ChatId
-import dao.UserId
+import dao.*
 import model.*
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class Server {
+class Server : KoinComponent {
+    val userBase: UserDao by inject()
+    val chatBase: ChatDao by inject()
+    val messageBase: MessageDao by inject()
+    val blockedUsersBase: BlockedUsersDao by inject()
+    val chatsOfUserBase: ChatsOfUserDao by inject()
+    val membersOfGroupChatBase: MembersOfGroupChatDao by inject()
+    val contactsOfUserBase: ContactsOfUserDao by inject()
+
     fun authenticate(userId: UserId) {
         //return token
     }
+
     fun register(userId: UserId) {}
-    fun getChats(userId: UserId): List<Chat> { return emptyList() }
-    fun getPersonalChats(userId: UserId): List<PersonalChat> { return emptyList() }
-    fun getGroupChats(userId: UserId): List<GroupChat> { return emptyList() }
-    fun getContacts(userId: UserId): List<Contact> { return emptyList() }
-    fun getChatMessages(id: ChatId): List<Message> { return emptyList() }
+    fun getChats(userId: UserId): List<Chat> {
+        return emptyList()
+    }
+
+    fun getPersonalChats(userId: UserId): List<PersonalChat> {
+        return emptyList()
+    }
+
+    fun getGroupChats(userId: UserId): List<GroupChat> {
+        return emptyList()
+    }
+
+    fun getContacts(userId: UserId): List<Contact> {
+        return emptyList()
+    }
+
+    fun getChatMessages(id: ChatId): List<Message> {
+        return emptyList()
+    }
 
     fun sendMessage(id: ChatId) {}
 
-    fun createGroupChat(id: UserId, name: String): ChatId { return 0 }
-    fun createPersonalChat(id: UserId, name: String): ChatId { return 0 }
-    
+    fun createGroupChat(id: UserId, name: String): ChatId {
+        return 0
+    }
+
+    fun createPersonalChat(id: UserId, name: String): ChatId {
+        return 0
+    }
+
 
 }
