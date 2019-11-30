f1f7aa3ef8341c00ee5863d0cfe7a91999f3f223
Nikita Bosov <neckbosov@gmail.com>
2019-11-30 23:41:39 +0300

    fix bugs in deletion of GroupChat

--- src/main/kotlin/databases/GroupChatDB.kt
+++ src/main/kotlin/databases/GroupChatDB.kt
@@ -4,19 +4,28 @@ import dao.GroupChatDao
 import dao.Id
 import dao.UserId
 import model.GroupChat
+import model.GroupChatToUser
 import model.User
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.transactions.transaction
 import tables.GroupChats
+import tables.GroupChatsToUsers
+import tables.getEntityID
 
 
 class GroupChatDB : GroupChatDao {
     override fun addNewGroupChat(owner: UserId, chatName: String, uniqueLink: String?) =
         transaction {
-            val ownerID = User.findById(owner)?.id ?: return@transaction null
+            val ownerID = getEntityID<User>(owner) ?: return@transaction null
             GroupChat.new {
                 this.owner = ownerID
                 this.chatName = chatName
                 this.uniqueLink = uniqueLink
+            }.also {
+                GroupChatToUser.new {
+                    this.chatId = it.id
+                    this.userId = ownerID
+                }
             }
         }
 
@@ -24,7 +33,11 @@ class GroupChatDB : GroupChatDao {
         transaction { GroupChat.findById(elemId) }
 
     override fun deleteById(elemId: Id) =
-        transaction { GroupChat.findById(elemId)?.delete() ?: Unit }
+        transaction {
+            val chat = GroupChat.findById(elemId) ?: return@transaction
+            GroupChatsToUsers.deleteWhere { GroupChatsToUsers.chatId eq chat.id }
+            chat.delete()
+        }
 
     override val size: Int
         get() = transaction { GroupChat.all().count() }
