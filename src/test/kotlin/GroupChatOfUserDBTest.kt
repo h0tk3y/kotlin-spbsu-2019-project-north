4ac14909a532449c00cb31c91da8f5bc2daa09bb
AlyaNovikova <aleksnovikova@gmail.com>
2019-11-30 23:03:16 +0300

    some tests for GroupChatOfUserDB

--- src/test/kotlin/GroupChatOfUserDBTest.kt
+++ src/test/kotlin/GroupChatOfUserDBTest.kt
@@ -1,42 +1,186 @@
+import dao.GroupChatDao
 import dao.GroupChatsOfUserDao
+import dao.UserDao
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.koin.test.inject
 
 class GroupChatOfUserDBTest : DBTest {
     @Test
-    fun addAndContainsTest() {
+    fun addTest() {
         val base: GroupChatsOfUserDao by inject()
+        val chats: GroupChatDao by inject()
+        val users: UserDao by inject()
 
-        base.add(1, 1)
-        base.add(1, 2)
-        base.add(2, 1)
-        Assertions.assertTrue(base.contains(1, 1))
-        Assertions.assertTrue(base.contains(1, 2))
-        Assertions.assertTrue(base.contains(2, 1))
+        val alya = users.addNewUser(
+            "Alya",
+            "Alya@gmail.com",
+            "1234567",
+            "Pingwin",
+            "123"
+        )
+
+        val vanya = users.addNewUser(
+            "Vanya",
+            "Vanya@gmail.com",
+            "8888888",
+            "olivva",
+            "8"
+        )
+
+        val antoha = users.addNewUser(
+            "Antoha",
+            "Antoha@gmail.com",
+            "4444444",
+            "Kartoha",
+            "4444"
+        )
+
+
+        val chat1 = chats.addNewGroupChat(alya.id.value, "Alin chat", "knskcsa")
+        val chat2 = chats.addNewGroupChat(vanya.id.value, "Vanin chat", "oqlmcznka")
+
+        if (chat1?.id?.value != null) {
+            Assertions.assertTrue(base.add(vanya.id.value, chat1.id.value))
+            Assertions.assertTrue(base.add(antoha.id.value, chat1.id.value))
+        }
+
+        if (chat2?.id?.value != null)
+            Assertions.assertTrue(base.add(alya.id.value, chat2.id.value))
+
+        Assertions.assertFalse(base.add(alya.id.value, 239))
+
     }
 
     @Test
     fun removeTest() {
         val base: GroupChatsOfUserDao by inject()
+        val chats: GroupChatDao by inject()
+        val users: UserDao by inject()
 
-        base.add(1, 1)
-        base.add(1, 2)
-        base.add(2, 1)
-        base.remove(2, 1)
-        Assertions.assertTrue(base.contains(1, 1))
-        Assertions.assertTrue(base.contains(1, 2))
-        Assertions.assertFalse(base.contains(2, 1))
+        val alya = users.addNewUser(
+            "Alya",
+            "Alya@gmail.com",
+            "1234567",
+            "Pingwin",
+            "123"
+        )
+
+        val vanya = users.addNewUser(
+            "Vanya",
+            "Vanya@gmail.com",
+            "8888888",
+            "olivva",
+            "8"
+        )
+
+        val antoha = users.addNewUser(
+            "Antoha",
+            "Antoha@gmail.com",
+            "4444444",
+            "Kartoha",
+            "4444"
+        )
+
+
+        val chat1 = chats.addNewGroupChat(alya.id.value, "Alin chat", "knskcsa")
+
+        if (chat1?.id?.value != null) {
+            Assertions.assertTrue(base.add(vanya.id.value, chat1.id.value))
+            Assertions.assertTrue(base.add(antoha.id.value, chat1.id.value))
+
+            Assertions.assertTrue(base.remove(vanya.id.value, chat1.id.value))
+            Assertions.assertFalse(base.remove(vanya.id.value, chat1.id.value))
+        }
+
+        Assertions.assertFalse(base.remove(alya.id.value, 239))
     }
 
     @Test
     fun selectTest() {
         val base: GroupChatsOfUserDao by inject()
+        val chats: GroupChatDao by inject()
+        val users: UserDao by inject()
 
-        base.add(1, 1)
-        base.add(1, 2)
-        base.add(2, 1)
-        Assertions.assertTrue(base.select(1).size == 2)
-        Assertions.assertTrue(base.select(2).size == 1)
+        val alya = users.addNewUser(
+            "Alya",
+            "Alya@gmail.com",
+            "1234567",
+            "Pingwin",
+            "123"
+        )
+
+        val vanya = users.addNewUser(
+            "Vanya",
+            "Vanya@gmail.com",
+            "8888888",
+            "olivva",
+            "8"
+        )
+
+        val antoha = users.addNewUser(
+            "Antoha",
+            "Antoha@gmail.com",
+            "4444444",
+            "Kartoha",
+            "4444"
+        )
+
+
+        val chat1 = chats.addNewGroupChat(alya.id.value, "Alin chat", "knskcsa")
+        val chat2 = chats.addNewGroupChat(vanya.id.value, "Vanin chat", "oqlmcznka")
+
+        if (chat1?.id?.value != null)
+            Assertions.assertTrue(base.add(vanya.id.value, chat1.id.value))
+        if (chat1?.id?.value != null)
+            Assertions.assertTrue(base.add(antoha.id.value, chat1.id.value))
+        if (chat2?.id?.value != null)
+            Assertions.assertTrue(base.add(alya.id.value, chat2.id.value))
+
+        Assertions.assertFalse(base.add(alya.id.value, 239))
+    }
+
+    @Test
+    fun containsTest() {
+        val base: GroupChatsOfUserDao by inject()
+        val chats: GroupChatDao by inject()
+        val users: UserDao by inject()
+
+        val alya = users.addNewUser(
+            "Alya",
+            "Alya@gmail.com",
+            "1234567",
+            "Pingwin",
+            "123"
+        )
+
+        val vanya = users.addNewUser(
+            "Vanya",
+            "Vanya@gmail.com",
+            "8888888",
+            "olivva",
+            "8"
+        )
+
+        val antoha = users.addNewUser(
+            "Antoha",
+            "Antoha@gmail.com",
+            "4444444",
+            "Kartoha",
+            "4444"
+        )
+
+
+        val chat1 = chats.addNewGroupChat(alya.id.value, "Alin chat", "knskcsa")
+        val chat2 = chats.addNewGroupChat(vanya.id.value, "Vanin chat", "oqlmcznka")
+
+        if (chat1?.id?.value != null)
+            Assertions.assertTrue(base.add(vanya.id.value, chat1.id.value))
+        if (chat1?.id?.value != null)
+            Assertions.assertTrue(base.add(antoha.id.value, chat1.id.value))
+        if (chat2?.id?.value != null)
+            Assertions.assertTrue(base.add(alya.id.value, chat2.id.value))
+
+        Assertions.assertFalse(base.add(alya.id.value, 239))
     }
 }
