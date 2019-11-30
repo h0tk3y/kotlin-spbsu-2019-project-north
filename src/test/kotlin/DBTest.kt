508de92f46d0bf3d238b00529ed274c94b04411a
AlyaNovikova <aleksnovikova@gmail.com>
2019-11-30 10:54:22 +0300

    add fun before all tests

--- src/test/kotlin/DBTest.kt
+++ src/test/kotlin/DBTest.kt
@@ -1,17 +1,37 @@
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
+import tables.*
 
 interface DBTest : KoinTest {
     @BeforeEach
     fun start() {
         startKoin { modules(daoModule) }
+        transaction {
+            SchemaUtils.create(Contacts)
+            SchemaUtils.create(GroupChats)
+            SchemaUtils.create(GroupChatsToUsers)
+            SchemaUtils.create(Messages)
+            SchemaUtils.create(PersonalChats)
+            SchemaUtils.create(Users)
+        }
     }
+
 
     @AfterEach
     fun stop() {
         stopKoin()
+    }
+
+}
+
+object ConnectDB {
+    init {
+        Database.connect("jdbc:h2:mem:regular", "org.h2.Driver")
     }
 }
