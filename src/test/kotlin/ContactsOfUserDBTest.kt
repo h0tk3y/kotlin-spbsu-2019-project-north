0868d92c3f0679951c513520ae86538d8d606ad7
AlyaNovikova <aleksnovikova@gmail.com>
2019-12-01 18:25:43 +0300

    tests for Contacts Of User DB

--- src/test/kotlin/ContactsOfUserDBTest.kt
+++ src/test/kotlin/ContactsOfUserDBTest.kt
@@ -1,65 +1,151 @@
-//import dao.ContactsOfUserDao
-//import model.Contact
-//import org.junit.jupiter.api.Assertions
-//import org.junit.jupiter.api.Test
-//import org.koin.test.inject
-//
-//class ContactsOfUserDBTest : DBTest {
-//    @Test
-//    fun addAndContainsTest() {
-//        val base: ContactsOfUserDao by inject()
-//        val alya = Contact(1, "Alya")
-//        val antoha = Contact(2, "Antoha")
-//
-//        base.add(1, antoha)
-//        base.add(2, alya)
-//
-//        Assertions.assertTrue(base.contains(1, antoha))
-//        Assertions.assertTrue(base.contains(2, alya))
-//        Assertions.assertFalse(base.contains(1, alya))
-//    }
-//
-//    @Test
-//    fun removeTest() {
-//        val base: ContactsOfUserDao by inject()
-//        val alya = Contact(1, "Alya")
-//        val antoha = Contact(2, "Antoha")
-//
-//        base.add(1, antoha)
-//        base.add(2, alya)
-//
-//        base.remove(1, antoha)
-//
-//        Assertions.assertFalse(base.contains(1, antoha))
-//        Assertions.assertTrue(base.contains(2, alya))
-//    }
-//
-//    @Test
-//    fun selectTest() {
-//        val base: ContactsOfUserDao by inject()
-//        val alya = Contact(1, "Alya")
-//        val antoha = Contact(2, "Antoha")
-//
-//        base.add(1, antoha)
-//        base.add(2, alya)
-//        base.add(1, alya)
-//
-//        Assertions.assertTrue(base.select(1).size == 2)
-//        Assertions.assertTrue(base.select(2).size == 1)
-//    }
-//
-//    @Test
-//    fun changeNameTest() {
-//        val base: ContactsOfUserDao by inject()
-//        val alya = Contact(1, "Alya")
-//        val antoha = Contact(2, "Antoha")
-//
-//        base.add(1, antoha)
-//        base.add(2, alya)
-//
-//        base.changeName(1, 2, "Kartoha")
-//
-//        Assertions.assertTrue(base.select(1)[0].name == "Kartoha")
-//        Assertions.assertTrue(base.select(2)[0].name == "Alya")
-//    }
-//}
+import dao.BlockedUsersDao
+import dao.ContactsOfUserDao
+import dao.UserDao
+import model.Contact
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.koin.test.inject
+
+class ContactsOfUserDBTest : DBTest {
+    @Test
+    fun addAndContainsTest() {
+        val base: ContactsOfUserDao by inject()
+        val users: UserDao by inject()
+
+        val alya = users.addNewUser(
+            "Alya",
+            "Alya@gmail.com",
+            "1234567",
+            "Pingwin",
+            "123"
+        ).id.value
+
+        val nikita = users.addNewUser(
+            "Nikita",
+            "Nikita@gmail.com",
+            "5553535",
+            "Nikita",
+            "55555"
+        ).id.value
+
+        val antoha = users.addNewUser(
+            "Antoha",
+            "Antoha@gmail.com",
+            "4444444",
+            "Kartoha",
+            "4444"
+        ).id.value
+
+        Assertions.assertTrue(base.add(alya, Pair(nikita, "Nikita")))
+        Assertions.assertTrue(base.add(alya, Pair(antoha, "Kartoha")))
+        Assertions.assertTrue(base.add(antoha, Pair(alya, "kek")))
+
+        Assertions.assertTrue(base.contains(alya, Pair(nikita, "Nikita")))
+        Assertions.assertTrue(base.contains(alya, Pair(antoha, "Kartoha")))
+        Assertions.assertTrue(base.contains(antoha, Pair(alya, "kek")))
+
+        Assertions.assertTrue(base.contains(alya, Pair(nikita, "Bosov")))
+        Assertions.assertTrue(base.contains(alya, Pair(antoha, "KOKOKOKOKOKOKOKOKO")))
+
+        Assertions.assertFalse(base.contains(nikita, Pair(alya, "kek")))
+    }
+
+    @Test
+    fun removeAndSelectTest() {
+        val base: ContactsOfUserDao by inject()
+        val users: UserDao by inject()
+
+        val alya = users.addNewUser(
+            "Alya",
+            "Alya@gmail.com",
+            "1234567",
+            "Pingwin",
+            "123"
+        ).id.value
+
+        val nikita = users.addNewUser(
+            "Nikita",
+            "Nikita@gmail.com",
+            "5553535",
+            "Nikita",
+            "55555"
+        ).id.value
+
+        val antoha = users.addNewUser(
+            "Antoha",
+            "Antoha@gmail.com",
+            "4444444",
+            "Kartoha",
+            "4444"
+        ).id.value
+
+        Assertions.assertTrue(base.add(alya, Pair(nikita, "Nikita")))
+        Assertions.assertTrue(base.add(alya, Pair(antoha, "Kartoha")))
+        Assertions.assertTrue(base.add(antoha, Pair(alya, "kek")))
+
+        Assertions.assertEquals(2, base.select(alya).size)
+        Assertions.assertTrue(base.select(alya).any{it.first == antoha})
+        Assertions.assertTrue(base.select(alya).any{it.first == nikita})
+
+        Assertions.assertEquals(1, base.select(antoha).size)
+        Assertions.assertTrue(base.select(antoha).any{it.first == alya})
+
+        Assertions.assertEquals(0, base.select(nikita).size)
+
+
+        Assertions.assertTrue(base.contains(alya, Pair(nikita, "Nikita")))
+        Assertions.assertTrue(base.contains(alya, Pair(antoha, "Kartoha")))
+
+        Assertions.assertTrue(base.remove(alya, Pair(nikita, "Nikita")))
+        Assertions.assertTrue(base.remove(alya, Pair(antoha, "KOKOKOKOKOKOKOKOKO")))
+
+        Assertions.assertFalse(base.contains(alya, Pair(nikita, "Nikita")))
+        Assertions.assertFalse(base.contains(alya, Pair(antoha, "Kartoha")))
+
+        Assertions.assertEquals(0, base.select(alya).size)
+        Assertions.assertEquals(1, base.select(antoha).size)
+        Assertions.assertEquals(0, base.select(nikita).size)
+    }
+
+    @Test
+    fun changeNameTest() {
+        val base: ContactsOfUserDao by inject()
+        val users: UserDao by inject()
+
+        val alya = users.addNewUser(
+            "Alya",
+            "Alya@gmail.com",
+            "1234567",
+            "Pingwin",
+            "123"
+        ).id.value
+
+        val nikita = users.addNewUser(
+            "Nikita",
+            "Nikita@gmail.com",
+            "5553535",
+            "Nikita",
+            "55555"
+        ).id.value
+
+        val antoha = users.addNewUser(
+            "Antoha",
+            "Antoha@gmail.com",
+            "4444444",
+            "Kartoha",
+            "4444"
+        ).id.value
+
+        Assertions.assertTrue(base.add(alya, Pair(nikita, "Nikita")))
+        Assertions.assertTrue(base.add(alya, Pair(antoha, "Kartoha")))
+        Assertions.assertTrue(base.add(antoha, Pair(alya, "kek")))
+
+        base.changeName(alya, antoha, "Antoha")
+        base.changeName(antoha, alya, "Alya")
+
+        Assertions.assertTrue(base.select(alya).any({it.second == "Antoha"}))
+        Assertions.assertFalse(base.select(alya).any({it.second == "Kartoha"}))
+        Assertions.assertTrue(base.select(antoha).any({it.second == "Alya"}))
+        Assertions.assertFalse(base.select(antoha).any({it.second == "kek"}))
+    }
+}
