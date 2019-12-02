4ae001ec206f2760f31ccbdf4ef1d24540008811
AlyaNovikova <aleksnovikova@gmail.com>
2019-12-02 15:11:21 +0300

    make the code beautifulgit add src/test/kotlin/PersonalChatDBTest.kt !

--- src/test/kotlin/PersonalChatDBTest.kt
+++ src/test/kotlin/PersonalChatDBTest.kt
@@ -16,7 +16,7 @@ class PersonalChatTests: DBTest {
             "1234567",
             "Pingwin",
             "123"
-        )
+        ).id.value
 
         val vanya = users.addNewUser(
             "Vanya",
@@ -24,11 +24,11 @@ class PersonalChatTests: DBTest {
             "8888888",
             "olivva",
             "8"
-        )
+        ).id.value
 
-        val chat1 = chats.addNewPersonalChat(alya.id.value, vanya.id.value)
-        val chat2 = chats.addNewPersonalChat(239, 30)
-        val chat3 = chats.addNewPersonalChat(alya.id.value, 239)
+        val chat1 = chats.addNewPersonalChat(alya, vanya)?.id?.value
+        val chat2 = chats.addNewPersonalChat(239, 30)?.id?.value
+        val chat3 = chats.addNewPersonalChat(alya, 239)?.id?.value
 
         Assertions.assertNotNull(chat1)
 
@@ -47,7 +47,7 @@ class PersonalChatTests: DBTest {
             "1234567",
             "Pingwin",
             "123"
-        )
+        ).id.value
 
         val vanya = users.addNewUser(
             "Vanya",
@@ -55,15 +55,15 @@ class PersonalChatTests: DBTest {
             "8888888",
             "olivva",
             "8"
-        )
+        ).id.value
 
-        val chat1 = chats.addNewPersonalChat(alya.id.value, vanya.id.value)
+        val chat1 = chats.addNewPersonalChat(alya, vanya)?.id?.value
 
-        Assertions.assertNotNull( chat1?.id?.value)
-        if (chat1?.id?.value != null) {
-            Assertions.assertEquals(chat1.id, chats.getById(chat1.id.value)?.id)
-            Assertions.assertEquals(alya.id.value, chats.getById(chat1.id.value)?.member1?.value)
-            Assertions.assertEquals(vanya.id.value, chats.getById(chat1.id.value)?.member2?.value)
+        Assertions.assertNotNull( chat1)
+        if (chat1 != null) {
+            Assertions.assertEquals(chat1, chats.getById(chat1)?.id?.value)
+            Assertions.assertEquals(alya, chats.getById(chat1)?.member1?.value)
+            Assertions.assertEquals(vanya, chats.getById(chat1)?.member2?.value)
         }
 
         Assertions.assertNull(chats.getById(239))
@@ -80,7 +80,7 @@ class PersonalChatTests: DBTest {
             "1234567",
             "Pingwin",
             "123"
-        )
+        ).id.value
 
         val vanya = users.addNewUser(
             "Vanya",
@@ -88,17 +88,17 @@ class PersonalChatTests: DBTest {
             "8888888",
             "olivva",
             "8"
-        )
+        ).id.value
 
-        val chat1 = chats.addNewPersonalChat(alya.id.value, vanya.id.value)
+        val chat1 = chats.addNewPersonalChat(alya, vanya)?.id?.value
 
-        Assertions.assertNotNull(chat1?.id?.value)
-        if (chat1?.id?.value != null) {
-            Assertions.assertEquals(chat1.id, chats.getById(chat1.id.value)?.id)
+        Assertions.assertNotNull(chat1)
+        if (chat1 != null) {
+            Assertions.assertEquals(chat1, chats.getById(chat1)?.id?.value)
 
-            chats.deleteById(chat1.id.value)
+            chats.deleteById(chat1)
 
-            Assertions.assertNull(chats.getById(chat1.id.value)?.id)
+            Assertions.assertNull(chats.getById(chat1)?.id)
         }
 
     }
@@ -114,7 +114,7 @@ class PersonalChatTests: DBTest {
             "5553535",
             "Nikita",
             "55555"
-        )
+        ).id.value
 
         val vanya = users.addNewUser(
             "Vanya",
@@ -122,7 +122,7 @@ class PersonalChatTests: DBTest {
             "8888888",
             "olivva",
             "8"
-        )
+        ).id.value
 
         val antoha = users.addNewUser(
             "Antoha",
@@ -130,27 +130,27 @@ class PersonalChatTests: DBTest {
             "4444444",
             "Kartoha",
             "4444"
-        )
+        ).id.value
 
-        val chat1 = chats.addNewPersonalChat(nikita.id.value, vanya.id.value)
-        val chat2 = chats.addNewPersonalChat(nikita.id.value, antoha.id.value)
-        val chat3 = chats.addNewPersonalChat(antoha.id.value, vanya.id.value)
+        val chat1 = chats.addNewPersonalChat(nikita, vanya)?.id?.value
+        val chat2 = chats.addNewPersonalChat(nikita, antoha)?.id?.value
+        val chat3 = chats.addNewPersonalChat(antoha, vanya)?.id?.value
 
-        Assertions.assertNotNull(chat1?.id?.value)
-        Assertions.assertNotNull(chat2?.id?.value)
-        Assertions.assertNotNull(chat3?.id?.value)
+        Assertions.assertNotNull(chat1)
+        Assertions.assertNotNull(chat2)
+        Assertions.assertNotNull(chat3)
 
         Assertions.assertEquals(3, chats.size)
-        if (chat1?.id?.value != null)
-            chats.deleteById(chat1.id.value)
+        if (chat1 != null)
+            chats.deleteById(chat1)
 
         Assertions.assertEquals(2, chats.size)
-        if (chat2?.id?.value != null)
-            chats.deleteById(chat2.id.value)
+        if (chat2 != null)
+            chats.deleteById(chat2)
 
         Assertions.assertEquals(1, chats.size)
-        if (chat3?.id?.value != null)
-            chats.deleteById(chat3.id.value)
+        if (chat3 != null)
+            chats.deleteById(chat3)
 
         Assertions.assertEquals(0, chats.size)
     }
